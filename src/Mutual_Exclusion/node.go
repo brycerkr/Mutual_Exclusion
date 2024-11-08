0ac7904df665e6adc89bdd3917aebe6a0a9af6aa
Bryce Karnikar <brka@itu.dk>
2024-11-08 18:06:25 +0100

    Program now starts 3 nodes and connects them to each other

--- src/Mutual_Exclusion/node.go
+++ src/Mutual_Exclusion/node.go
@@ -3,10 +3,10 @@ package main
 import (
 	pb "Mutual_Exclusion/m/v2/raalgo"
 	"context"
-	"fmt"
 	"log"
 	"net"
 	"sync"
+    "time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,19 +15,17 @@ import (
 //Node needs to keep list of other nodes in network
 //Look at map from ChittyChat - map of nodeid to stream
 
-
 type P2PNode struct {
-    pb.UnimplementedP2PnetworkServer
-    peers    map[string]pb.P2PnetworkClient // map of peer addresses to clients
-    peerLock sync.RWMutex
+	pb.UnimplementedP2PnetworkServer
+	peers    map[string]pb.P2PnetworkClient // map of peer addresses to clients
+	peerLock sync.RWMutex
 }
 
 // Server method implementation
 func (n *P2PNode) SendMessage(ctx context.Context, req *pb.Request) (*pb.Reply, error) {
-    log.Printf("Received message from %d at time %d\n", req.Nodeid, req.Timestamp)
-    return &pb.Reply{Permission: true}, nil
+	log.Printf("Received message from %d at time %d\n", req.Nodeid, req.Timestamp)
+	return &pb.Reply{Permission: true}, nil
 }
-
 
 //Below is an implementation of a GetStatus rpc method that we don't have in our .proto
 //Returns which nodes a node is currently connected to
@@ -51,60 +49,80 @@ func (n *P2PNode) GetStatus(ctx context.Context, req *pb.Empty) (*pb.StatusRespo
 
 // Add a peer to the node
 func (n *P2PNode) AddPeer(address string) {
-    conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
-        log.Printf("Failed to connect to peer %s: %v", address, err)
-        return
-    }
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Printf("Failed to connect to peer %s: %v", address, err)
+		return
+	}
 
-    client := pb.NewP2PnetworkClient(conn)
+	client := pb.NewP2PnetworkClient(conn)
 
-    n.peerLock.Lock()
-    n.peers[address] = client
-    n.peerLock.Unlock()
+	n.peerLock.Lock()
+	n.peers[address] = client
+	n.peerLock.Unlock()
 
-    log.Printf("Connected to peer %s", address)
+	log.Printf("Connected to peer %s", address)
+}
+
+func StartServer(node *P2PNode, address string) {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterP2PnetworkServer(grpcServer, node)
+
+	log.Printf("P2P node is running on port %s/n", address)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
 
 func main() {
-    node := &P2PNode{
-        peers: make(map[string]pb.P2PnetworkClient),
-    }
+	node1 := &P2PNode{
+		peers: make(map[string]pb.P2PnetworkClient),
+	}
+    node2 := &P2PNode{
+		peers: make(map[string]pb.P2PnetworkClient),
+	}
+    node3 := &P2PNode{
+		peers: make(map[string]pb.P2PnetworkClient),
+	}
 
-    // Start the gRPC server
-    go func() {
-        lis, err := net.Listen("tcp", ":50051")
-        if err != nil {
-            log.Fatalf("Failed to listen: %v", err)
-        }
+	// Start the gRPC server
+	go StartServer(node1, ":50051")
+    go StartServer(node2, ":50052")
+    go StartServer(node3, ":50053")
 
-        grpcServer := grpc.NewServer()
-        pb.RegisterP2PnetworkServer(grpcServer, node)
+    time.Sleep(3 * time.Second)
 
-        log.Println("P2P node is running on port 50051")
-        if err := grpcServer.Serve(lis); err != nil {
-            log.Fatalf("Failed to serve: %v", err)
-        }
-    }()
+	// Add peers (simulate peer discovery for demonstration)
+	node1.AddPeer("localhost:50052") // Example of connecting to another node
+	node1.AddPeer("localhost:50053")
 
-    // Add peers (simulate peer discovery for demonstration)
-    node.AddPeer("localhost:50052") // Example of connecting to another node
-    node.AddPeer("localhost:50053")
+    node2.AddPeer("localhost:50051") // Example of connecting to another node
+	node2.AddPeer("localhost:50053")
 
-    // Simulate sending a message to a peer
-    node.peerLock.RLock()
-    if peer, exists := node.peers["localhost:50052"]; exists {
-        res, err := peer.Ask(context.Background(), &pb.Request{
-            Nodeid: 1,
-            Timestamp: timestamp,
-        })
-        if err != nil {
-            log.Printf("Error sending message: %v", err)
-        } else {
-            log.Printf("Response from peer: %s", res.Permission)
-        }
-    }
-    node.peerLock.RUnlock()
+    node3.AddPeer("localhost:50051") // Example of connecting to another node
+	node3.AddPeer("localhost:50052")
 
-    select {} // Keep the main function running
+	// Simulate sending a message to a peer
+	node1.peerLock.RLock()
+	if peer, exists := node1.peers["localhost:50052"]; exists {
+		res, err := peer.Ask(context.Background(), &pb.Request{
+			Nodeid:    1, //pass in
+			Timestamp: 2, //keep track
+		})
+		if err != nil {
+			log.Printf("Error sending message: %v", err)
+		} else {
+			if res.Permission {
+                log.Print("reply received")
+            }
+		}
+	}
+	node1.peerLock.RUnlock()
+
+	select {} // Keep the main function running
 }
